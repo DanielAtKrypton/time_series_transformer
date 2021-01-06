7ed55eca2e31417446a6a4d7b90e8f3d76ebcf7d
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-06 00:16:56 +0000

    :bug: Fix main test to really set the device properly.

--- tests/main_test.py
+++ tests/main_test.py
@@ -11,19 +11,18 @@ from flights_time_series_dataset import FlightsDataset
 from time_series_predictor import TimeSeriesPredictor
 from time_series_transformer import Transformer
 
-# @pytest.mark.skip
-def test_transformer_tsp():
-    """
-    Tests the TransformerTimeSeriesPredictor
-    """
-    if not torch.cuda.is_available():
-        pytest.skip("needs a CUDA compatible GPU available to run this test")
+@pytest.mark.parametrize('device', ['cuda', 'cpu'])
+def test_transformer_tsp(device):
+    if device == 'cuda':
+        if not torch.cuda.is_available():
+            pytest.skip("needs a CUDA compatible GPU available to run this test")
 
     start = time.time()
     tsp = TimeSeriesPredictor(
         Transformer(),
         max_epochs=50,
         train_split=None,
+        device=device,
     )
 
     tsp.fit(FlightsDataset())
@@ -31,24 +30,4 @@ def test_transformer_tsp():
     assert score > -1
     end = time.time()
     elapsed = timedelta(seconds = end - start)
-    print("Fitting in GPU time delta: {}".format(elapsed))
-
-# @pytest.mark.skip
-def test_transformer_tsp_in_cpu():
-    """
-    Tests the TransformerTimeSeriesPredictor
-    """
-    start = time.time()
-    tsp = TimeSeriesPredictor(
-        Transformer(),
-        max_epochs=50,
-        train_split=None,
-        device='cpu',
-    )
-
-    tsp.fit(FlightsDataset())
-    score = tsp.score(tsp.dataset)
-    assert score > -1
-    end = time.time()
-    elapsed = timedelta(seconds = end - start)
-    print("Fitting in CPU time delta: {}".format(elapsed))
+    print("Fitting in {} time delta: {}".format(device, elapsed))
